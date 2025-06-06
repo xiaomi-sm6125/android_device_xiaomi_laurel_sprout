1e7d05cca81fe8c26da88be4dc28e7ebb469b118
RainySorcerer <beastdark704@gmail.com>
2025-06-06 22:10:14 +0530

    laurel_sprout: Aosify
    Signed-off-by: RainySorcerer <beastdark704@gmail.com>

--- aosp_laurel_sprout.mk
+++ aosp_laurel_sprout.mk
@@ -8,13 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from laurel_sprout device
 $(call inherit-product, device/xiaomi/laurel_sprout/device.mk)
 
-PRODUCT_NAME := lineage_laurel_sprout
+# Andriod-One Flags
+TARGET_SUPPORT_MINIMAL_GAPPS := true
+TARGET_HAS_GEMENI_BOOTANIMATION := false
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_SUPPORT_LIVE_WALLPAPER := false
+GMS_VOICE_MODEL_INCLUDED := false
+
+PRODUCT_NAME := aosp_laurel_sprout
 PRODUCT_DEVICE := laurel_sprout
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
